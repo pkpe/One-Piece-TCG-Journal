61689bf3f0c0abe1d5b619b2e0cfa0f1dc07c4da
pkpe <94029038+pkpe@users.noreply.github.com>
2025-09-16 13:37:38 -1000

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import OnePieceJournal from './components/OnePieceJournal';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <OnePieceJournal />
+    </div>
+  );
+}
+
+export default App;
